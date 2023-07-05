f3463b3cb9867e81e5f41fbff44e285695135bb7
kwm0304 <106484883+kwm0304@users.noreply.github.com>
2023-07-05 19:57:24 -0400

    initial server and routes, controller next

---
+++ backend/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	- "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+type Task struct {
+	gorm.Model
+	Title			string
+	Completed	bool
+	UserId		uint
+}
+
+func main() {
+	//connect to db
+	db, err := gorm.Open("sqlite3", "todo.db")
+	if err != nil {
+		log.Fatal("Unable to connect to db:", err)
+	}
+	defer db.Close()
+
+	//auto-migrate the trask struct to create the 'task' table
+	db.AutoMigrate(&Task{})
+
+	//create new gin router
+	router := gin.Default()
+
+	//enable cors middleware
+	router.Use(cors.Default())
+
+	//define routes
+	router.POST("/tasks", createTask)
+	router.GET("/tasks", getTasks)
+	router.PUT("/tasks/:id", updateTask)
+	router.DELETE("/tasks/:id", deleteTask)
+
+	//starting http server
+	log.Println("Server started on localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
